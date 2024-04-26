e8e7de855257c3655ab93861aa9a29924a4c5324
Isnen Hadi <89633584+isnen27@users.noreply.github.com>
2024-04-26 07:11:30 +0700

    Update astatine.py
    Initial libraries

--- astatine.py
+++ astatine.py
@@ -1,6 +1,26 @@
-# Libraries
-import streamlit as st
-import pandas as pd
+# for basic operations
 import numpy as np
+import pandas as pd
+import streamlit as st
+
+#for visualizations
+import matplotlib.pyplot as plt
+import math
+import seaborn as sns
+from pandas import plotting
+import matplotlib.style as style
+style.use("fivethirtyeight")
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import matplotlib.gridspec as grid_spec
+%matplotlib inline
+import missingno as msno
 
 # Extended File
+from eda import
+from model import
+from prepo import
+
+
